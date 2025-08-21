675121a63b7cd468dd95b7f6f68c306576ce111d
Leon <ch4maeleon@naver.com>
2025-08-22 02:51:04 +0900

    feat: 지도 화면 짤리는 현상 개선 상단바, 하단봐 관련 설정 추가

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import * as Location from 'expo-location';
 import { useEffect, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Platform, StyleSheet, View } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default function HomeScreen() {
   const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const tabBarHeight = useBottomTabBarHeight();
 
   useEffect(() => {
     (async () => {
@@ -40,14 +42,14 @@ export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <MapView
-        style={styles.map}
+        style={[styles.map, { marginBottom: tabBarHeight }]}
         provider={PROVIDER_GOOGLE}
         region={currentRegion}
         showsUserLocation={true}
         showsMyLocationButton={true}
         showsCompass={true}
         zoomEnabled={true}
-        zoomControlEnabled={true}
+        zoomControlEnabled={Platform.OS === 'android'}
         followsUserLocation={location !== null}
       />
     </View>
@@ -57,9 +59,9 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: Platform.OS === 'ios' ? 44 : 24, 
   },
   map: {
-    width: '100%',
-    height: '100%',
+    flex: 1,
   },
 });
